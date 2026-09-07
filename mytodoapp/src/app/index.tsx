5fe440a27de3b2645baa3d2c71df9d57babf1afe
macdee501 <62038430+macdee501@users.noreply.github.com>
2026-09-07 21:22:46 +0200

    added some interface

--- mytodoapp/src/app/index.tsx
+++ mytodoapp/src/app/index.tsx
@@ -1,6 +1,9 @@
-import { Text, View, StyleSheet } from "react-native";
+import { useState } from "react";
+import { Text, View, StyleSheet, TextInput, TouchableOpacity } from "react-native";
 
 export default function HomeScreen() {
+  // Creates temporary memory to be used later
+  const [task, setTask] =useState("")
   return (
     // Controls what you see on screen, its like a fragment
     <View style={styles.container}>
@@ -9,8 +12,17 @@ export default function HomeScreen() {
             My Tasks
       </Text>
       <Text style={styles.subtitle}>
-        I have no taskks yet
+        What needs doing??
+        <TextInput style={styles.input} placeholder="Enter a task" value={task} onChangeText={setTask}/>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Add  Task</Text>
+        </TouchableOpacity>
+
+        <Text style={styles.preview}>{task}</Text>
       </Text>
+      {/* <Text style={styles.subtitle}>
+        I have no taskks yet
+      </Text> */}
     </View>
     
   );
@@ -35,7 +47,31 @@ const styles = StyleSheet.create({
   subtitle:{
     fontSize:8,
     marginTop:8,
+  },
+  input:{
+    borderWidth:1,
+    borderColor:"#cccccc",
+    borderRadius:10,
+    padding:14,
+    fontSize:16,
+  },
+  button:{
+    backgroundColor:"#111111",
+    padding:14,
+    borderRadius:10,
+    marginTop:12,
+    alignItems:"center",
+  },
+  buttonText:{
+    color:'#ffffff',
+    fontSize:16,
+    fontWeight:'bold'
+  },
+  preview:{
+    marginTop:20,
+    fontSize:18,
   }
+
 })
 
 
