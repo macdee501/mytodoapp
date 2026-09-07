e9a7a9b93ebfcc1cd767831b11742fcde187ff79
macdee501 <62038430+macdee501@users.noreply.github.com>
2026-09-07 21:03:51 +0200

    Created basic home screen

--- mytodoapp/src/app/index.tsx
+++ mytodoapp/src/app/index.tsx
@@ -1,17 +1,41 @@
 import { Text, View, StyleSheet } from "react-native";
 
-export default function Index() {
+export default function HomeScreen() {
   return (
+    // Controls what you see on screen, its like a fragment
     <View style={styles.container}>
-      <Text>Edit src/app/index.tsx to edit this screen.</Text>
+      {/* Text is the tag for texts */}
+      <Text style={styles.title}>
+            My Tasks
+      </Text>
+      <Text style={styles.subtitle}>
+        I have no taskks yet
+      </Text>
     </View>
+    
   );
 }
 
+// acts as css file for the current page file
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+  container:{
+    // Makes content fit entire screen
+    flex:1,
+    // puts padding around the screen
+    padding:24,
+    // puts content at the center of the screen
+    justifyContent:'center'
   },
-});
+  title:{
+    fontSize:32,
+    fontWeight:'bold',
+    
+
+  },
+  subtitle:{
+    fontSize:8,
+    marginTop:8,
+  }
+})
+
+
